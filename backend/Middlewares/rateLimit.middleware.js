27e1ddbc1e5633d1d6e47c51f8a6440d33a55d30
FiorellaSF <afiosf@gmail.com>
2024-04-29 09:01:26 +0200

    Add node index.js

--- backend/Middlewares/rateLimit.middleware.js
+++ backend/Middlewares/rateLimit.middleware.js
@@ -4,7 +4,7 @@ import rateLimit from 'express-rate-limit';
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 5,
-  message: 'Demasiados intentos de inicio de sesión, por favor intenta de nuevo más tarde.',
+  message: 'Demasiados intentos, por favor intenta de nuevo más tarde.',
 });
 
 const loginLimiter = rateLimit({
--- backend/Routes/Auth.routes.js
+++ backend/Routes/Auth.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { login, register } from "../Controllers/Auth.controller.js";
-// import { loginLimiter } from "../Middlewares/rateLimit.middleware.js";
+import { loginLimiter } from "../Middlewares/rateLimit.middleware.js";
 const router = express.Router();
 
 
-// router.use(loginLimiter);
+router.use(loginLimiter);
 
 router.post('/register', register);
 router.post('/login', login);
